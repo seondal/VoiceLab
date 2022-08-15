fe057a471ba8c2da88b12c8e737c10b51384d690
seondal <sseondal@gmail.com>
2022-08-15 18:37:34 +0900

    [Feat] Edit for text version

--- main.js
+++ main.js
@@ -1,72 +1,186 @@
-const audio_files = [
+const text_files = [
+  // batch 0
   [
-    // batch0
-    "./stim/KsponSpeech_125921_4sec.wav",
-    "./stim/KsponSpeech_125922_4sec.wav",
-    "./stim/KsponSpeech_125924_4sec.wav",
+    "왜 안 맞지?",
+    "선생님, 다른 방법은요?",
+    "저기 왼쪽으로 쭉 가시면 돼요.",
+    "손 좀 씻어.",
+    "쓰레기 냄새.",
+    "너무 다행이다.",
+    "많이 아플까요?",
+    "잠깐 지나갈게요.",
+    "날씨도 나처럼 우울하네.",
+    "왜 꼭 바쁠 때 그래?",
+    "일단 진정 좀 하세요.",
+    "보여주지 마. 그런 거.",
+    "부모가 저렇게 키우니까 애들이 버릇이 없는 거야.",
+    "두 시에 올 거면 집은 뭐 하러 와.",
+    "앞에, 앞에!",
+    "몇 개야, 이게 다?",
   ],
+  // batch 1
   [
-    // batch1
-    "./stim/KsponSpeech_125925_4sec.wav",
-    "./stim/KsponSpeech_125927_4sec.wav",
-    "./stim/KsponSpeech_125929_4sec.wav",
+    "계속 피곤해야겠다.",
+    "친구랑 같이 가면 안 돼요?",
+    "이러면 안 되는데.",
+    "네. 인상 좋은 거 알아요.",
+    "돈 내고 써야 돼요?",
+    "두고 봅시다. 당신들.",
+    "손님. 못 바꿔드려요.",
+    "너 나 좀 보자.",
+    "건강이 최고야.",
+    "배고파. 내 것도 같이 해줘.",
+    "행사 언제까지죠?",
+    "우리 끝입니까?",
+    "많이 아플까요?",
+    "그렇게 됐다.",
+    "너무 위험해.",
+    "수술 잘 된 것 맞죠?",
   ],
+  // batch 2
   [
-    // batch2
-    "./stim/KsponSpeech_125933_4sec.wav",
-    "./stim/KsponSpeech_125934_4sec.wav",
-    "./stim/KsponSpeech_125935_4sec.wav",
+    "뭔 소리야?",
+    "나 술 안 마셔 이제.",
+    "집에 어떻게 가냐?",
+    "학생이야 산악인이야.",
+    "잡히기만 해, 진짜.",
+    "하루종일 집에 있으면서 대체 뭐 했어?",
+    "정상까지 얼마나 남았어요?",
+    "빠진 적이 있어서.",
+    "왜 이렇게 깊어?",
+    "너나 잘해라, 너나.",
+    "당장 나가.",
+    "오늘 입고 나가야지.",
+    "못하면 어떡하지?",
+    "오늘 내가 다 산다.",
+    "지들 자식이라면서 똥은 왜 안 치워.",
+    "이러면 안 되는데.",
+    "내 옷 입지 말라 그랬지.",
   ],
+  // batch 3
   [
-    // batch3
-    "./stim/KsponSpeech_125944_4sec.wav",
-    "./stim/KsponSpeech_125946_4sec.wav",
-    "./stim/KsponSpeech_125949_4sec.wav",
+    "너무 귀여워. 어떡해.",
+    "어제까진 잘 됐었는데, 갑자기 안 되네요.",
+    "나 이제 누구랑 놀아?",
+    "왜 아무것도 안 보여?",
+    "저거 나야?",
+    "내가 봉이냐?",
+    "뭔 일 있었어?",
+    "쪽팔린 줄도 모르고.",
+    "여기 땅 전부?",
+    "아직 언제 끝날지 모르겠어요.",
+    "벌써 전역했어?",
+    "별 미친 놈을 다 보겠네.",
+    "멧돼지가 가게 안으로 들어왔어요.",
+    "저는 그쪽한테 관심 없다니까요.",
+    "이거 쓰고 어떻게 살았어?",
+    "이제 다 컸네.",
   ],
+  // batch 4
   [
-    // batch4
-    "./stim/KsponSpeech_125952_4sec.wav",
-    "./stim/KsponSpeech_125954_4sec.wav",
-    "./stim/KsponSpeech_125955_4sec.wav",
+    "난 취직 언제 하냐.",
+    "저걸 혼자 다 먹어?",
+    "못해요. 그냥 내려갈게요.",
+    "너무 진짜 같아.",
+    "집에 어떻게 가냐?",
+    "지금 시간이 몇 시인데 뛰어다녀요?",
+    "언제까지 이렇게 사냐.",
+    "실감이 안 나.",
+    "해고라니요.",
+    "상쾌하다.",
+    "이제 다 컸네.",
+    "뭔 소리야, 이게?",
+    "얘기 들어줘서 고맙다.",
+    "머리카락이 녹아요?",
+    "큰일 날 뻔 했네.",
+    "정말 믿어지지가 않네요.",
   ],
+  // batch 5
   [
-    // batch5
-    "./stim/KsponSpeech_125960_4sec.wav",
-    "./stim/KsponSpeech_125961_4sec.wav",
-    "./stim/KsponSpeech_125962_4sec.wav",
+    "머리 진짜 작다.",
+    "완전히 망했다.",
+    "또 지각이다.",
+    "오늘 공강이었어.",
+    "후회 안 하실 거죠.",
+    "얼마만의 바다야.",
+    "차가워.",
+    "밤 되면 여기도 무섭구나.",
+    "저 왔어요.",
+    "이제 다 컸네.",
+    "사진 좀 찍어 주시면 안 될까요.",
+    "너도 걸리면 어쩌려고.",
+    "진작 만날 걸 그랬어요.",
+    "집에 어떻게 가냐?",
+    "드디어 붙었어.",
+    "제발 백만 아니길.",
   ],
+  // batch 6
   [
-    // batch6
-    "./stim/KsponSpeech_125966_4sec.wav",
-    "./stim/KsponSpeech_125968_4sec.wav",
-    "./stim/KsponSpeech_125971_4sec.wav",
+    "드디어 샀다.",
+    "나 먼저 갈게.",
+    "먹어본 것 중에 제일 맛있어.",
+    "빨리 와, 빨리.",
+    "드라이브 갈래?",
+    "너 여기 왜 있어?",
+    "몇 시나 됐지?",
+    "몇 개야, 이게 다?",
+    "계속 피곤해야겠다.",
+    "무슨 광고가 이렇게 길어?",
+    "고양이 풀어놓지 말라고.",
+    "당신 뭐야?",
+    "우산이 필요가 없잖아.",
+    "한잔 더 하러 갈 사람.",
+    "어디 갔어?",
+    "웬일이냐, 내 걱정을 다 하고.",
   ],
+  // batch 7
   [
-    // batch7
-    "./stim/KsponSpeech_125972_4sec.wav",
-    "./stim/KsponSpeech_125973_4sec.wav",
-    "./stim/KsponSpeech_125984_4sec.wav",
+    "뭐 하고 놀까.",
+    "맵기만 하구만.",
+    "이게 죽으라는 거지.",
+    "오늘 보자고 하려고 했지.",
+    "몇 개야, 이게 다?",
+    "여기도 닫았네.",
+    "진짜 왔나봐.",
+    "해고라니요.",
+    "거미줄이 왜 이렇게 많아.",
+    "착하지 우리 아들. 조금만 참자.",
+    "나 지금 도착했어.",
+    "배고픈데 라면 먹자.",
+    "오늘은 할 일이 있어서.",
+    "당신 뭐야?",
+    "진짜 없어. 어떡해.",
+    "너 때문에 초기화됐잖아.",
   ],
+  // batch 8
   [
-    // batch8
-    "./stim/KsponSpeech_125985_4sec.wav",
-    "./stim/KsponSpeech_125987_4sec.wav",
-    "./stim/KsponSpeech_125989_4sec.wav",
+    "걔가 한 말 다 거짓말이었어?",
+    "언제 다시 길러.",
+    "이번 한 번만 도와주십시오.",
+    "정상까지 얼마나 남았어요?",
+    "새우부터 해동시켜.",
+    "일단 진정 좀 하세요.",
+    "무슨 소리야, 이게.",
+    "열심히 키운 보람이 있네.",
+    "완전히 망했어.",
+    "쟤는 좋을까 슬플까?",
+    "정말 열심히 했는데.",
+    "이 회의 주제가 뭔지는 알고 말하시는 거죠?",
+    "오늘 할 일이 있어서.",
+    "왜 이렇게 많이 빠졌지?",
+    "뭐지, 이게?",
+    "오늘 할 일이 있어서.",
   ],
+  // batch 9
   [
-    // batch9
-    "./stim/KsponSpeech_125992_4sec.wav",
-    "./stim/KsponSpeech_125994_4sec.wav",
-    "./stim/KsponSpeech_126000_4sec.wav",
-  ],
-  [
-    "./stim/angry10_067-279.wav",
-    "./stim/happiness10_009-008.wav",
-    "./stim/sad10_001-312.wav",
+    "시청자 여러분, 안녕하십니까.",
+    "안녕히 주무세요.",
+    "왜 자꾸 거짓말을 하세요?",
+    "손님. 못 바꿔드려요.",
+    "빨리 집으로 와.",
+    "후회 안 하실 거죠.",
   ],
 ];
-const test_audio = ["./stim/z_volumeTest.wav"];
-const beep_audio = ["./stim/z_beep2s.wav"];
 
 // random batch number
 const random10 = Math.floor(Math.random() * 10);
@@ -79,8 +193,8 @@ const counterBalance = random2;
 counterBalance ? console.log("trialA") : console.log("trialB");
 
 // set variables - part1
-const audioQ = jsPsych.randomization.shuffle(audio_files[batchNum]);
-const numTrials = audio_files[batchNum].length;
+const audioQ = jsPsych.randomization.shuffle(text_files[batchNum]);
+const numTrials = text_files[batchNum].length;
 const catchTrial = [
   math.floor(math.random((numTrials * 2) / 8, (3 * numTrials) / 8)),
   math.floor(math.random((6 * numTrials) / 8, (7 * numTrials) / 8)),
@@ -307,21 +421,7 @@ expTL.push({
   },
 });
 
-// Volume test
-expTL.push({
-  type: "html-button-response",
-  stimulus: [
-    '<p style="text-align: left; margin: auto">Before we begin, please adjust your earphone volume. A sample voice will be played when you press Next.' +
-      "<br>Use that sound to set the volume to the level you feel comfortable.<br><br></p>",
-  ],
-  choices: ["Next"],
-});
-
-expTL.push({
-  type: "audio-likert-jedit_volumeAdjust",
-  stimulus: test_audio,
-  preamble: "<p>Adjust your system volume.<br></p>",
-});
+// Intro - 안내 글자는 추후 직접 추가해주세요
 
 expTL.push({
   type: "html-button-response",
@@ -341,10 +441,7 @@ for (let t = 0; t < numTrials; t++) {
     timeline: [
       {
         type: "html-keyboard-response",
-        stimulus:
-          '<audio autoplay><source src="' +
-          audioQ[t] +
-          '", type = "audio/wav">Your browser does not support the audio element.</audio>',
+        stimulus: audioQ[t],
         choices: jsPsych.NO_KEYS,
         response_ends_trial: false,
         trial_duration: 5000,
@@ -492,7 +589,6 @@ jsPsych.init({
   show_progress_bar: true,
   timeline: expTL,
   use_webaudio: false,
-  preload_audio: [audio_files, test_audio, beep_audio],
   preload_images: [catchImgs],
   on_finish: function () {
     jsPsych.data.displayData();
